3c28bfc81a12d6cbdb8364505a241aa14987571e
yllgashi <yllxgashi@gmail.com>
2021-02-20 02:19:30 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Important',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Important'
+            ),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
+  }
+}
