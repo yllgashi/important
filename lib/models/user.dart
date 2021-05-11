3d35dddfeda1d3b9d78f5e4f0ab1e649a03e030a
yllxgashi <yll@kutia.net>
2021-05-11 13:39:09 +0200

    add user class

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  String firstName;
+  String lastName;
+  String email;
+  String password;
+
+  User({
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.password,
+  });
+}
